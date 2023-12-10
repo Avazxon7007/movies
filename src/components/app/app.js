60afd09938252e6b9197130febd5aef373075393
Aliy <aliy01@gmail.com>
2023-12-10 13:49:38 +0500

    yangilash

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,6 +3,7 @@ import "./app.css"
 import AppInfo from "../app-info/app-info"
 import SearchPanel from "../search-panel/search-panel"
 import AppFiler from "../app-filter/app-filter"
+import MovieList from "../movie-list/movie-list"
 
 const App = () => {
     return (
@@ -15,6 +16,9 @@ const App = () => {
         </div>
         <div>
             <AppFiler/>
+        </div>
+        <div>
+            <MovieList/>
         </div>
     </div>
     )
